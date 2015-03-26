a8feb328f91d90a92f2e3af42ab7415d6a1cf9b9
dkotwica <domk123@gmail.com>
2015-03-26 10:24:44 -0400

    test

--- app/src/main/java/domdomdom/myfirstapp/Browse.java
+++ app/src/main/java/domdomdom/myfirstapp/Browse.java
@@ -4,38 +4,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class Browse extends ActionBarActivity {
 
-    //Test Change
+    HashMap<String, List<String>> Movies_category;
+    List<String> Movies_list;
+    ExpandableListView Exp_list;
+    BeerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+        Exp_list = (ExpandableListView) findViewById(R.id.exp_list);
+        Movies_category = BeerData.getInfo();
+        Movies_list = new ArrayList<String>(Movies_category.keySet());
+        adapter = new BeerAdapter(this, Movies_category, Movies_list);
+        Exp_list.setAdapter(adapter);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_browse, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
