04fe8288ca33813e1f91a1b6a8d7bce6e8f27fd4
dkotwica <domk123@gmail.com>
2015-03-26 14:17:20 -0400

    Hashmap

---
+++ app/src/main/java/domdomdom/myfirstapp/BeerAdapter.java
@@ -0,0 +1,109 @@
+package domdomdom.myfirstapp;
+
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.MonthDisplayHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+public class BeerAdapter extends BaseExpandableListAdapter{
+    private Context ctx;
+    private HashMap<String, List<String>> Movies_Category;
+    private List<String> Movies_List;
+
+    public BeerAdapter(Context ctx, HashMap<String, List<String>> Movies_Category, List<String> Movies_List )
+    {
+        this.ctx = ctx;
+        this.Movies_Category = Movies_Category;
+        this.Movies_List = Movies_List;
+
+    }
+
+    @Override
+    public Object getChild(int parent, int child) {
+
+
+        return Movies_Category.get(Movies_List.get(parent)).get(child);
+    }
+
+    @Override
+    public long getChildId(int parent, int child) {
+        // TODO Auto-generated method stub
+        return child;
+    }
+
+    @Override
+    public View getChildView(int parent, int child, boolean lastChild, View convertview,
+                             ViewGroup parentview)
+    {
+        String child_title =  (String) getChild(parent, child);
+        if(convertview == null)
+        {
+            LayoutInflater inflator = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertview = inflator.inflate(R.layout.child_layout, parentview,false);
+        }
+        TextView child_textview = (TextView) convertview.findViewById(R.id.child_txt);
+        child_textview.setText(child_title);
+
+
+        return convertview;
+    }
+
+    @Override
+    public int getChildrenCount(int arg0) {
+
+        return Movies_Category.get(Movies_List.get(arg0)).size();
+    }
+
+    @Override
+    public Object getGroup(int arg0) {
+        // TODO Auto-generated method stub
+        return Movies_List.get(arg0);
+    }
+
+    @Override
+    public int getGroupCount() {
+        // TODO Auto-generated method stub
+        return Movies_List.size();
+    }
+
+    @Override
+    public long getGroupId(int arg0) {
+        // TODO Auto-generated method stub
+        return arg0;
+    }
+
+    @Override
+    public View getGroupView(int parent, boolean isExpanded, View convertview, ViewGroup parentview) {
+        // TODO Auto-generated method stub
+        String group_title = (String) getGroup(parent);
+        if(convertview == null)
+        {
+            LayoutInflater inflator = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertview = inflator.inflate(R.layout.parent_layout, parentview,false);
+        }
+        TextView parent_textview = (TextView) convertview.findViewById(R.id.parent_txt);
+        parent_textview.setTypeface(null, Typeface.BOLD);
+        parent_textview.setText(group_title);
+        return convertview;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int arg0, int arg1) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
---
+++ app/src/main/java/domdomdom/myfirstapp/BeerData.java
@@ -0,0 +1,206 @@
+package domdomdom.myfirstapp;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by domk1_000 on 3/25/2015.
+ */
+public class BeerData {
+    public static HashMap<String, List<String>> getInfo()
+    {
+        HashMap<String, List<String>> MoviesDetails = new HashMap<String, List<String>>();
+
+        List<String> Swamp_Head = new ArrayList<String>();
+        Swamp_Head.add("Swamphead Big Nose"); // hoppy
+        Swamp_Head.add("Swamphead Wild Night"); // ale
+        Swamp_Head.add("Swamphead Cottonmouth"); // wheat
+        Swamp_Head.add("Swamphead Midnight Oil"); // stout
+        Swamp_Head.add("Swamphead Stumpknocker"); // hoppy
+        Swamp_Head.add("Swamp Head River Logger"); // lager
+        Swamp_Head.add("Barley Mow The Unkindness"); // ale
+        Swamp_Head.add("Swamp Head Down Deep"); // brown
+        Swamp_Head.add("Engine 15 Nut Sack"); // ale
+        Swamp_Head.add("Swamp Head Batch 800 PB&J"); // ale
+        Swamp_Head.add("Swamp Head Irish Red"); // ale
+        Swamp_Head.add("Swamp Head Tropical Disturbance"); // hoppy
+        Swamp_Head.add("Swamp Head Devil's Nettle"); // fruity
+        Swamp_Head.add("Swamp Head Saison Du Swamp with Brett"); // saison
+
+
+
+        List<String> First_Magnitude = new ArrayList<String>();
+        First_Magnitude.add("First Magnitude 72 Pale Ale"); // hoppy
+        First_Magnitude.add("First Magnitude Drift"); // ale
+        First_Magnitude.add("First Magnitude Siren"); // blonde
+        First_Magnitude.add("First Magnitude Ursa IPA"); // hoppy
+        First_Magnitude.add("The Minnow Imperial Stout"); // stout
+        First_Magnitude.add("Scott's Session IPA"); // hoppy
+        First_Magnitude.add("The Tube ESB"); // hoppy
+        First_Magnitude.add("First Magnitude Karst"); // wheat
+        First_Magnitude.add("First Magnitude Time Out Dry"); // stout
+
+
+        List<String> World_Beer = new ArrayList<String>();
+        World_Beer.add("Ace Apple Cider"); // sour
+        World_Beer.add("Ace Joker Cider"); // sour
+        World_Beer.add("Anderson Valley Dark Side of the Boon"); // ale
+        World_Beer.add("Anderson Valley Fall Hornin'"); // ale
+        World_Beer.add("Anderson Valley the Kimmie"); // fruity
+        World_Beer.add("Bell's Two Hearted Ale"); // hoppy
+        World_Beer.add("Chimay Premiere"); // wheat
+        World_Beer.add("Cigar City Hotter than Helles"); // lager
+        World_Beer.add("First Magnitude 72 Pale Ale"); // hoppy
+        World_Beer.add("First Magnitude Siren"); // blonde
+        World_Beer.add("Florida Beer Swamp Ape IPA"); // hoppy
+        World_Beer.add("George Killian's Irish Red"); // ale
+        World_Beer.add("Guinness Draught"); // ale
+        World_Beer.add("Innis & Gunn Scottish Porter"); // porter
+        World_Beer.add("Intuition Ale Works Boneshaker"); // lager
+        World_Beer.add("Jdub's Brewing Uptop IPA"); // hoppy
+        World_Beer.add("Kona Big Wave"); // blonde
+        World_Beer.add("Left Hand Milk Stout"); // nitro
+        World_Beer.add("Lindemans Framboise"); // fruity
+        World_Beer.add("Lost Coast Downtown Brown"); // brown
+        World_Beer.add("Lucifer"); // wheat
+        World_Beer.add("New Blegium Snapshot"); // wheat
+        World_Beer.add("Pilsner Urquell - Hladinka"); // pilsner
+        World_Beer.add("Pilsner Urquell - Mliko"); // pilsner
+        World_Beer.add("Pilsner Urquell - Snyt"); // pilsner
+        World_Beer.add("Red Brick 21st Anniversary"); // saison
+        World_Beer.add("Rivertown Hop Bomber"); // hoppy
+        World_Beer.add("Rivrtown Roebling Porter"); // porter
+        World_Beer.add("Samuel Adams Cold Snap"); // fruity
+        World_Beer.add("Sea Dog Wild Blueberry Wheat Ale"); // wheat
+        World_Beer.add("Sierra Nevada Nooner"); // pilsner
+        World_Beer.add("St. Bernardus Abt 12"); // wheat
+        World_Beer.add("Swamp Head Big Nose"); // hoppy
+        World_Beer.add("Swamp Head Cotton Mouth"); // wheat
+        World_Beer.add("Swamp Head Stump Knocker"); // hoppy
+        World_Beer.add("Tripel Van De Garre"); // wheat
+        World_Beer.add("Two Brothers Monarch White Beer"); // wheat
+        World_Beer.add("Two Brothers Northwind Imperial Stout"); // stout
+        World_Beer.add("Unibroue Terrible"); // ale
+        World_Beer.add("Victory Donnybrook Stout Nitro"); // stout
+        World_Beer.add("Weihenstephaner Hefeweissbier Dunkel"); // dunkel
+        World_Beer.add("Weihenstephaner Pilsner"); // pilsner
+
+
+
+        List<String> House_Beer = new ArrayList<String>();
+        House_Beer.add("Sprecher Bootleggers Hard Root Beer"); // lager
+        House_Beer.add("Angry Orchard Crisp Apple Cider"); // sour
+        House_Beer.add("Cigar City Jai Alai IPA"); // hoppy
+        House_Beer.add("Weihenstephaner Korbinian"); //wheat
+        House_Beer.add("Black Diamond Rampage Imperial IPA"); // hoppy
+        House_Beer.add("Founders Imperial Stout"); // stout
+        House_Beer.add("Destihl Hoperation Overload"); // hoppy
+        House_Beer.add("Ommegang Scythe + Sickle"); // fruity
+        House_Beer.add("Fat Heads Bumble Berry"); // fruity
+        House_Beer.add("CCB Cigar City Hard Cider"); // sour
+        House_Beer.add("Ommegang Fire and Blood"); // ale
+        House_Beer.add("Smuttynose Gravitation 2015"); // wheat
+        House_Beer.add("Weyerbacher Heresy"); // stout
+        House_Beer.add("Ommegang Valar Morghulis"); // wheat
+        House_Beer.add("Bold City Rhino Rye Pale Ale"); // hoppy
+        House_Beer.add("Lost Coast Tangerine Wheat"); // fruity
+        House_Beer.add("Full Pint TRI-PA"); // hoppy
+        House_Beer.add("Great Divide Yeti"); // stout
+        House_Beer.add("Ommegang Hop House"); // hoppy
+        House_Beer.add("Oskar Blues G-Knight"); // ale
+        House_Beer.add("Cigar City Maduro Brown Ale"); // brown
+        House_Beer.add("Bells Java Stout"); // stout
+        House_Beer.add("Dogfish Head ApriHop"); // hoppy
+        House_Beer.add("JDubs Poolside"); // wheat
+        House_Beer.add("To Ol Garden of Eden");
+        House_Beer.add("Schneider Weisse Original (Tap 7)"); //weizen
+        House_Beer.add("Rodenbach Grand Cru"); // ale
+        House_Beer.add("First Magnitude Siren"); // blonde
+        House_Beer.add("Great Divide Orabelle"); // wheat
+        House_Beer.add("Moonlight Desire"); // mead
+        House_Beer.add("Innis + Gunn Rum Finish Oak Aged"); // ale
+        House_Beer.add("Gearge Killians Irish Red"); // ale
+        House_Beer.add("St. Louis Framboise"); // fruity
+        House_Beer.add("Left Hand Oak Aged Widdershins"); // ale
+        House_Beer.add("Anderson Valley Blood Orange Gose"); // fruity
+        House_Beer.add("Magners Original Cide"); // sour
+        House_Beer.add("Guiness Draught"); // nitro
+        House_Beer.add("Victory Donnybrook Stout"); // stout
+        House_Beer.add("Left Hand Milk Stout Nitro"); // nitro
+        House_Beer.add("Cigar City Patio Tools"); // stout
+
+
+        List<String> Crafty_Bastard = new ArrayList<String>();
+        Crafty_Bastard.add("Guiness Draught"); // nitro
+        Crafty_Bastard.add("Left Hand Milk Stout"); // nitro
+        Crafty_Bastard.add("Southern Tier 2x Milk Stout"); // nitro
+        Crafty_Bastard.add("Bouldershake Chocolate Porter"); //nitro
+        Crafty_Bastard.add("Sierra Nevada Hop Hunter"); // hoppy
+        Crafty_Bastard.add("Abita Purple Haze"); // fruity
+        Crafty_Bastard.add("Curious Traveler"); // (fruity?)
+        Crafty_Bastard.add("New Belgium Fat Tire"); // hoppy , cirtrus
+        Crafty_Bastard.add("New Belgium Snapshot Wheat"); // Wheat
+        Crafty_Bastard.add("Terrapin Rye Pale Ale"); // Hoppy
+        Crafty_Bastard.add("Lagunitas Daytime IPA"); // hoppy
+        Crafty_Bastard.add("Swamp Head Midnight Oil"); // stout
+        Crafty_Bastard.add("Swamp Head Cottonmouth"); // wheat
+        Crafty_Bastard.add("Swamp Head Morning Glow"); // blonde
+        Crafty_Bastard.add("Swamp Head Big Nose IPA"); // hoppy
+        Crafty_Bastard.add("Angry Orcchar Crisp Apple Cider"); // sour
+        Crafty_Bastard.add("Boulevard Single-Wide IPA"); // hoppy
+        Crafty_Bastard.add("Stone Arrogant Bastard Ale"); // hoppy
+        Crafty_Bastard.add("Founders Imperial Stout"); // stout
+        Crafty_Bastard.add("Cigar City Maudro Brown Ale"); // brown
+        Crafty_Bastard.add("Southern Tier Sonnet"); // saison
+        Crafty_Bastard.add("Southern Tier Live"); // hoppy
+        Crafty_Bastard.add("Southern Tier Right-O-Way"); // hoppy
+        Crafty_Bastard.add("Southern Tier 2x IPA"); // hoppy
+        Crafty_Bastard.add("Southern Tier Unearthly"); // hoppy
+        Crafty_Bastard.add("Southern Tier Goat Boy");
+        Crafty_Bastard.add("Southern Tier 2xPRESSO"); // sout sweet
+        Crafty_Bastard.add("Southern Tier Choklat"); // stout sweet
+        Crafty_Bastard.add("Southern Tier Pumpking"); // Ale
+        Crafty_Bastard.add("Southern Tier Creme Brulee"); // stout
+        Crafty_Bastard.add("Southern Tier Mokah"); // stout
+        Crafty_Bastard.add("Weihenstephaner Hefe Weissbier"); // weizen
+        Crafty_Bastard.add("Smuttynose Robust Porter"); // porter
+        Crafty_Bastard.add("Wittekerke Wit"); // wheat
+        Crafty_Bastard.add("Florida Beer Swamp Ape"); // hoppy
+        Crafty_Bastard.add("Lost Coast Sharkinator"); // blonde
+        Crafty_Bastard.add("St. Louis Framboise"); // fruity
+        Crafty_Bastard.add("Blackthorn Dry Cider"); // sour
+        Crafty_Bastard.add("Frankiskaner Hefe-Weisse Dunkel"); // dunkel
+        Crafty_Bastard.add("Bold City roxy's finest"); // blonde
+        Crafty_Bastard.add("First Magnitude Karst"); // hoppy
+        Crafty_Bastard.add("First Magnitude 72 Pale ALe"); // hoppy
+        Crafty_Bastard.add("First Magnitude Siren"); // blonde
+        Crafty_Bastard.add("First Magnitude Drift"); // stout
+        Crafty_Bastard.add("Green Flash Hop Odyssey Symposium IPA"); // hoppy
+        Crafty_Bastard.add("Woodchuck Amber Draft Cider"); // sour
+        Crafty_Bastard.add("Victory DirtWolf"); // hoppy
+        Crafty_Bastard.add("Goose Island 312 Urban Wheat"); // wheat
+        Crafty_Bastard.add("Anderson Valley Blood Orange Gose"); // fruity
+        Crafty_Bastard.add("Wynwood Pop's Porter"); // porter
+        Crafty_Bastard.add("Shock Top Belgian White"); // wheat
+        Crafty_Bastard.add("Green Flash Double Stout"); // stout
+        Crafty_Bastard.add("Two Bros Monarch White Beer"); // wheat
+        Crafty_Bastard.add("Lexington Bourbon Barrel Stout"); //stout
+        Crafty_Bastard.add("Rivertown Roebling Imperial Robust Porter"); // porter
+        Crafty_Bastard.add("5 Rabbit Magnifica Wheat"); // wheat
+        Crafty_Bastard.add("Sixpoint Bengali Tiger IPA"); // hoppy
+
+
+
+        MoviesDetails.put("World of Beer", World_Beer);
+        MoviesDetails.put("Swamphead Brewery", Swamp_Head);
+        MoviesDetails.put("First Magnitude Brewery", First_Magnitude);
+        MoviesDetails.put("Crafty Bastard", Crafty_Bastard);
+        MoviesDetails.put("House of Beer", House_Beer);
+
+        return MoviesDetails;
+
+    }
+
+}
